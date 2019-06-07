846ac02a1e8c7521022ef556bc720e53a1bd227a
Jhong098 <Jhong098@gmail.com>
2019-06-06 23:40:12 -0400

    editable flyTo animation params

--- src/Map.js
+++ src/Map.js
@@ -1,46 +1,96 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 import mapboxgl from "mapbox-gl/dist/mapbox-gl";
 import maxBoxGeocoder from "@mapbox/mapbox-gl-geocoder";
 
-class Map extends React.Component {
-  searchBox = null;
-  map = null;
+const Map = () => {
+  const defaultParams = {
+    bearing: 0,
+    zoom: 10,
+    pitch: 40,
+    curve: 1.5,
+    speed: 2
+  };
+  mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API;
+  const [animParams, setAnimParams] = useState(defaultParams);
+  const { bearing, zoom, pitch, curve, speed } = animParams;
+  
+  const bearingRef = React.useRef();
+  const zoomRef = React.useRef();
+  const pitchRef = React.useRef();
+  const curveRef = React.useRef();
+  const speedRef = React.useRef();
 
-  componentDidMount() {
-    console.log(process.env.REACT_APP_MAPBOX_API)
-    mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API;
-    this.map = new mapboxgl.Map({
-        container: 'mapbox',
-        style: 'mapbox://styles/mapbox/streets-v11',
-        zoom: 10,
-        center: [-80.5416618, 43.4728746]
+  const map = React.useRef();
+
+  useEffect(() => {
+    console.log('initialized map');
+    map.current = new mapboxgl.Map({
+      container: 'mapbox',
+      style: process.env.REACT_APP_MAPBOX_STYLE,
+      zoom: 10,
+      center: [-80.5416618, 43.4728746]
     });
-    const geocoder = new maxBoxGeocoder({
+  }, []);
+
+  useEffect(() => {
+    console.log("changed geocoder params");
+    if (map.current) {
+      const geocoder = new maxBoxGeocoder({
         accessToken: mapboxgl.accessToken,
         types: "country, region, place",
-        flyTo: {
-            bearing: 90,
-            zoom: 10,
-            pitch: 40,
-            curve: 1.5,
-            speed: 2,
-        },
-        minLength: 5,
-        
+        flyTo: animParams
+      });
+      const currChild = document.getElementById('geocoder').childNodes[0];
+      if (currChild) {
+        document.getElementById('geocoder').replaceChild(geocoder.onAdd(map.current), currChild);
+      } else {
+        document.getElementById('geocoder').appendChild(geocoder.onAdd(map.current));
+      }
+    }
+  }, [animParams]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(zoomRef.current.value);
+    setAnimParams({
+      bearing: bearingRef.current.value,
+      zoom: zoomRef.current.value,
+      pitch: pitchRef.current.value,
+      curve: curveRef.current.value,
+      speed: speedRef.current.value
     });
-    document.getElementById('geocoder').appendChild(geocoder.onAdd(this.map));
-  }
+  };
 
-  render() {
-    return (
-        <div>
-            <div id="mapbox" style={{ height: "500px", width: "100%" }} />
-            <div id="geocoder" />
-        </div>
-    );
-  }
-
-}
+  return (
+    <div>
+      <div id="mapbox" style={{ height: "500px", width: "100%" }} />
+      <div id="geocoder" />
+      <p>Map Animation Params</p>
+      <form onSubmit={onSubmit} style={{ display: "flex", flexDirection: "column", width: "200px" }}>
+        <label>
+          bearing: 
+          <input type="text" defaultValue={bearing} ref={bearingRef} />
+        </label>
+        <label>
+          zoom: 
+          <input type="text" defaultValue={zoom} ref={zoomRef} />
+        </label>
+        <label>
+          pitch: 
+          <input type="text" defaultValue={pitch} ref={pitchRef} />
+        </label>
+        <label>
+          curve: 
+          <input type="text" defaultValue={curve} ref={curveRef} />
+        </label>
+        <label>
+          speed: 
+          <input type="text" defaultValue={speed} ref={speedRef} />
+        </label>
+        <input type="submit" value="Apply" />
+      </form>
+    </div>
+  );
+};
 
 export default Map;
